65fe498846f5bc3fd65780eb07b271834f7bb9db
chawan <mlkchawan227@gmail.com>
2023-11-20 11:15:27 +0530

    commit2

--- src/main/java/org/cap/demo/Day2SpringBootRestApplication.java
+++ src/main/java/org/cap/demo/Day2SpringBootRestApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Day2SpringBootRestApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day2SpringBootRestApplication.class, args);
